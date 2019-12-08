f35f176f16fbb0af1f7a0f76eca6a8739da85738
jrb <jrb.youssef@gmail.com>
2019-12-08 16:29:02 +0100

    init commit

---
+++ database/migrations/2019_12_08_145906_create_auctions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuctionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auctions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('entry_price');
+            $table->boolean('is_live');
+            $table->integer('room_size')->nullable();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auctions');
+    }
+}
