db35c3f1635d2798de3357c91f3e31cf5a8c8981
jrb <jrb.youssef@gmail.com>
2019-12-10 09:59:38 +0100

    back: create auth api routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,23 @@ Route::resource('product', 'Product\ProductController');
 // auction resource routes
 Route::resource('auction', 'Auction\AuctionController');
 
+
+/** ==== Authentication endpoints ===== **/
+Route::prefix('auth')->group(function () {
+    // register
+    Route::post('register', 'AuthController@register');
+    // login
+    Route::post('login', 'AuthController@login');
+    // refresh token
+    Route::get('refresh', 'AuthController@refresh');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        // get the user
+        Route::get('user', 'AuthController@user');
+        // logout
+        Route::post('logout', 'AuthController@logout');
+    });
+});
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
