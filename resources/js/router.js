c68b51e138d59c3cc007551f2812b60d0435bc6b
jrb <jrb.youssef@gmail.com>
2019-12-10 10:02:34 +0100

    front: create routes for admin

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,4 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 
 /** ===== Components ===== **/
 import Welcome from './components/Welcome.vue';
@@ -8,30 +7,92 @@ import AuctionDetails from './components/Auction/AuctionDetails.vue';
 // Login
 import Login from './components/Auth/Login.vue';
 
-// Route middleware
-Vue.use(Router);
+/**
+ * Admin backend Routes!
+ */
+import AdminIndex from './components/Admin/Index.vue';
+import AdminProducts from './components/Admin/Products/index.vue';
+import AdminAuctions from './components/Admin/Auctions/index.vue';
+
+
+const routes = [
+    // Home route (welcome)
+    {
+        path: '/',
+        name: 'home',
+        component: Welcome,
+        meta: {
+            auth: undefined
+        }
+    },
+    // Show an Auction
+    {
+        path: '/auction/:id',
+        name: 'auction-details',
+        component: AuctionDetails,
+        meta: {
+            auth: undefined
+        }
+    },
+    // login
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            auth: false
+        }
+    },
+    // Admin backend (index)
+    {
+        path: '/admin/backend',
+        name: 'admin-backend',
+        component: AdminIndex,
+        meta: {
+            auth: {
+                roles: 1,
+                redirect: {
+                    name: 'login'
+                },
+                forbiddenRedirect: '/403'
+            }
+        }
+    },
+    // Admin backend (products)
+    {
+        path: '/admin/backend/products',
+        name: 'admin-products',
+        component: AdminProducts,
+        meta: {
+            auth: {
+                roles: 1,
+                redirect: {
+                    name: 'login'
+                },
+                forbiddenRedirect: '/403'
+            }
+        }
+    },
+    // Admin backend (auctions)
+    {
+        path: '/admin/backend/auctions',
+        name: 'admin-auctions',
+        component: AdminAuctions,
+        meta: {
+            auth: {
+                roles: 1,
+                redirect: {
+                    name: 'login'
+                },
+                forbiddenRedirect: '/403'
+            }
+        }
+    }
+];
 
 /** ===== Router declaration ===== **/
-export default new Router({
+export default new VueRouter({
+    history: true,
     mode: 'history',
-    routes: [
-        // Home route (welcome)
-        {
-            path: '/',
-            name: 'home',
-            component: Welcome
-        },
-        // Show an Auction
-        {
-            path: '/auction/:id',
-            name: 'auction-details',
-            component: AuctionDetails
-        },
-        // login
-        {
-            path: '/login',
-            name: 'login',
-            component: Login
-        }
-    ],
-});
+    routes,
+})
