717a3990dbda266c92fbabc55f6404eac5f1ea95
jrb <jrb.youssef@gmail.com>
2020-01-24 15:00:33 +0100

    front: add dynamic user name

--- resources/js/components/Template/DropdownLogin.vue
+++ resources/js/components/Template/DropdownLogin.vue
@@ -12,7 +12,7 @@
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
+            <v-list-item-title>{{this.$store.getters.currentUser.name}}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
