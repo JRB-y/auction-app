4b40201a903f2f56e75b7c6a68833279cae9957f
jrb <jrb.youssef@gmail.com>
2020-01-24 15:00:14 +0100

    front: avoid bet is undefined

--- resources/js/components/Auction/LiveAuction/BetHistory.vue
+++ resources/js/components/Auction/LiveAuction/BetHistory.vue
@@ -4,13 +4,16 @@
     <h4 class="title grey--text text--darken-3">Dernières mises</h4>
     <!-- ==== List Item Group ==== -->
     <v-list dense>
-      <v-list-item-group>
+      <v-list-item-group v-if="bets.lenght !== 0">
         <!--  :class="{'primary-active': index === 0}" -->
         <div v-for="(bet, index) in bets" :key="bet.title">
           <v-list-item :class="{'success': index === 0}">
             <v-list-item-content :class="{ 'white--text': index === 0 }">
               <!-- ==== User name ==== -->
-              <v-list-item-title>{{ index + 1 }}. {{bet.user.name}}</v-list-item-title>
+              <v-list-item-title>
+                {{ index + 1 }}. {{bet.user.name}}
+                <span v-if="bet.user.id === me.id">(vous)</span>
+              </v-list-item-title>
             </v-list-item-content>
             <!-- ==== Bet Price ==== -->
             <small class="subtitle" :class="{ 'white--text': index === 0 }">{{ bet.price }} CFA</small>
@@ -26,11 +29,21 @@ import moment from "moment";
 
 export default {
   name: "BetHistory",
-  props: ["bets"],
+  props: {
+    bets: {
+      default: [],
+      type: Array
+    }
+  },
   data() {
     return {
       moment: moment
     };
+  },
+  computed: {
+    me() {
+      return this.$store.getters.currentUser;
+    }
   }
 };
 </script>
